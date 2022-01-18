4e6225aa167694dd27434e4449dcc573f1129d4d
slee00 <slee000220@gmail.com>
2022-01-18 15:36:26 +0900

    java

---
+++ start/doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"start"}];updateSearchResults();
---
+++ start/src/start/Main.java
@@ -0,0 +1,11 @@
+package start;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n=2030;
+		System.out.println("Hello" + n);
+		System.out.print("Hello" + n);
+	}
+
+}
