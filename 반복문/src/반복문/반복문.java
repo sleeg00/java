61c0d358cc49eb8ff12e0ebee305f2a6af632cd0
slee00 <slee000220@gmail.com>
2022-01-20 17:13:22 +0900

    반복문 ,

---
+++ 반복문/src/반복문/반복문.java
@@ -0,0 +1,14 @@
+package 반복문;
+
+public class 반복문
+{
+
+	public static void main(String[] args)
+	{
+		int i=0;
+		for(i=0; i<10; i++, System.out.println(i))///,이후 실행가능 {}는 필수로 기입
+		{
+			
+		}
+	}
+}
